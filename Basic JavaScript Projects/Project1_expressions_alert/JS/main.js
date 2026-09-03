f0b9282f8fd5e28be2741886ea2f48ca74ac5a7f
huntersoto <134533676+huntersoto@users.noreply.github.com>
2026-09-02 19:30:54 -0500

    minor updates to my assignment submission
    Adding additional comments to my assignment submission

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -9,9 +9,9 @@ window.alert(wA);
 var x = "\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + " - Dr. Seuss",
     y = 'random words that won\'t be displayed. They simply serve as an example of assigninging multiple varibales in a single var statement',
     eX = '3+3',
-    Sent1 = "This is the beginning of the string",
-    Sent2 = ' and this is the end of the string';
-document.write(Sent1 + Sent2);
+    Sent1 = "This is the beginning of the string", //Setting the Sent1 variable
+    Sent2 = ' and this is the end of the string'; //Setting the Sent2 variable
+document.write(Sent1 + Sent2); //Concatentating Sent1 and Sent2 and displaying them using the document.write method
 
 //My First Function
 function My_First_Function()    {
