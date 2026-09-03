2ca67598a4b2117cf8e96e7f7791b6aee73b902a
huntersoto <134533676+huntersoto@users.noreply.github.com>
2026-09-02 20:15:00 -0500

    PROJECT2_FUNCTIONS ASSIGNMENT SUBMISSION
    PROJECT2_FUNCTIONS ASSIGNMENT SUBMISSION

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function myFunction()    {
+    var str1 = "This is the first variable", //Establishes the first variable
+        str2 = 'This is the second variable'; //Establishes the second variable
+        str1 += " and this is a string concatenated with the first string of the str1 variable" //Concatenates an additioanl string to the first string of the first variable
+    document.getElementById("Button_Text").innerHTML = str1; //Establishing the function using the first variable and Button_Text HTML ID
+    document.getElementById("Button2_Text").innerHTML = str2; //Establishing the function using the second varibale and Button2_Text HTML ID
+}
